120f934c7e890e0e55684204ab56de7e5a9169ba
jaosimass <jaovamorim@gmail.com>
2026-03-18 20:33:32 -0300

    first commit

---
+++ POO/treina-bank-api/src/main/java/br/com/treina/recife/bank/api/TreinaBankApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.treina.recife.bank.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinaBankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinaBankApiApplication.class, args);
+	}
+
+}
